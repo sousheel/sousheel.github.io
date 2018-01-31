96a1095e2531b15d721754952ea69c39887ae304
Sousheel Vunnam <soush915@gmail.com>
2018-01-30 20:03:05 -0700

    EHP Site

---
+++ scripts/wow.js
@@ -0,0 +1,14 @@
+anime({
+  targets: 'div',
+  translateX: 100,
+  duration: 2000,
+  loop: 3, // Play the animation 3 times
+  direction: 'alternate', // Play the animation in reverse
+});
+
+// var infiniteLoopAlternate = anime({
+//   targets: '#penis',
+//   translateX: 250,
+//   direction: 'alternate',
+//   loop: true
+// });
